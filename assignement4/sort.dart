42c06d106c636b6c99ccd28baf30063b4fc5fb5b
MustajabMustafa <mustajab.mustafa@gmail.com>
2023-03-04 01:39:47 +0500

    Another Commit

--- assignement4/sort.dart
+++ assignement4/sort.dart
@@ -3,10 +3,16 @@ void main() {
   print(
       "Q8: Write a program to sort a list of numbers. Program should be write using for loop only, .sort keyword is not allowed.[12,34,56,16,89,67,43]");
   List myList = [12, 34, 56, 16, 89, 67, 43];
-  List newList = [];
-  for (int i = myList.length - 1; i >= 0; i--) {
-    newList.add(myList[i]);
+  int temp = 0;
+  for (int i = 0; i < myList.length; i++) {
+    for (int j = i + 1; j < myList.length; j++) {
+      if (myList[i] > myList[j]) {
+        temp = myList[i];
+        myList[i] = myList[j];
+        myList[j] = temp;
+      }
+    }
   }
-  print(newList);
+  print(myList);
   print('');
 }
